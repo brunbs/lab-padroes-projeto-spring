e620a7ed4eebdae1d1f9f6ac831122eb3911d2fb
brunbs <babosa227@gmail.com>
2022-06-26 22:59:26 -0300

    Add models and repositories

--- src/main/java/one/digitalinnovation/labpadroesprojetospring/LabPadroesProjetoSpringApplication.java
+++ src/main/java/one/digitalinnovation/labpadroesprojetospring/LabPadroesProjetoSpringApplication.java
@@ -2,7 +2,19 @@ package one.digitalinnovation.labpadroesprojetospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * Spring Boot project built with Spring Initializr
+ * Modelus utilized:
+ * - Spring Data JPA
+ * - Spring Web
+ * - H2 Database
+ * - OpenFeign
+ *
+ * @author Bruno Barbosa
+ */
+@EnableFeignClients
 @SpringBootApplication
 public class LabPadroesProjetoSpringApplication {
 
